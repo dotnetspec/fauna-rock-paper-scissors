07f6352829cbdf60f82f53674f69779e1f98b62b
prmockridge@gmail.com <FK8v4nxB>
2021-03-15 16:18:17 +0800

    allPlayers

---
+++ fauna/functions/AllPlayers.ts
@@ -0,0 +1,21 @@
+import { query } from "faunadb";
+// import { RandomString, Switch } from "faunadb-fql-lib";
+// import DetermineResult from '../lib/DetermineResult';
+const { Lambda, Let, Query, Var, Select, Map, Paginate, Match, Index, Get } = query;
+
+
+const AllPlayers = {
+    //must match resolver name:
+    name: "all_players",
+    body: Query(
+        Lambda(
+            [],
+            Select(
+                "data",
+                Map(Paginate(Match(Index("allPlayers"))), Lambda("x", Get(Var("x"))))
+            )
+        )
+    ),
+};
+
+export = AllPlayers;
---
+++ fauna/functions/AllRankings.ts
@@ -0,0 +1,20 @@
+import { query } from "faunadb";
+// import { RandomString, Switch } from "faunadb-fql-lib";
+// import DetermineResult from '../lib/DetermineResult';
+const { Lambda, Let, Query, Var, Select, Map, Paginate, Match, Index, Get } = query;
+
+
+const AllRankings = {
+    name: "allRankings",
+    body: Query(
+        Lambda(
+            [],
+            Select(
+                "data",
+                Map(Paginate(Match(Index("allRankings"))), Lambda("x", Get(Var("x"))))
+            )
+        )
+    ),
+};
+
+export = AllRankings;
