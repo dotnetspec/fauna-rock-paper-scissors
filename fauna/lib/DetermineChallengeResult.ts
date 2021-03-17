2ea3da2124e0d892b4372cb5c58c370dba485943
prmockridge@gmail.com <FK8v4nxB>
2021-03-17 18:08:55 +0800

    use fauna LT function

--- fauna/lib/DetermineChallengeResult.ts
+++ fauna/lib/DetermineChallengeResult.ts
@@ -1,5 +1,5 @@
 import { ExprArg, ExprVal, query } from "faunadb";
-const { And, Equals, If, Or } = query;
+const { And, Equals, If, Or, LT } = query;
 
 export default function DetermineChallengeResult(selection: ExprArg, playerrank: ExprVal, opponentrank: ExprVal): ExprVal {
     return {
@@ -9,16 +9,14 @@ export default function DetermineChallengeResult(selection: ExprArg, playerrank:
         message: If(
             Equals(selection, "Abandoned"),
             "Abandoned!",
-            If(
-                Equals(selection, "Won"),
-                If(isWinnerHigherRank(), "You win!", "You lost in if"),
-                "You lose!"
-            )
+            //If(isWinnerHigherRank(playerrank, opponentrank), "You win in if!", "You lost in if")
+            If(LT(playerrank, opponentrank), "You win in if!", "You lost in if")
         )
     }
 }
 
+//nb. 'higher' rank is a lower int, so to app. playerrank must be less than oppentrank to be 'higher'
+// function isWinnerHigherRank(playerrank: ExprVal, opponentrank: ExprVal): Boolean {
+//     return If(LT(playerrank, opponentrank), true, false)
 
-function isWinnerHigherRank(): boolean {
-    return false && undefined;
-}
+// }
